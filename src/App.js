a5ef4fafad34660cb4eb29c1bf8fccfd28c5bfd5
Aaditya <48451686+Aaditya1007@users.noreply.github.com>
2025-03-26 17:57:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// src/App.js
-import React from "react";
-import BerserkerGame from "./BerserkerGame";
+import React from 'react';
+import BerserkerGame from './BerserkerGame';
+import './index.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-start p-4">
+    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-start p-10">
       <BerserkerGame />
     </div>
   );
