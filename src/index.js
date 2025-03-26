d71899257f6151bec911e8057321b4e7b1daad12
Aaditya <48451686+Aaditya1007@users.noreply.github.com>
2025-03-26 16:15:42 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // TailwindCSS or custom CSS
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
