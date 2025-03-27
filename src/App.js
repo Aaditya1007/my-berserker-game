ffd6779479d5a61ec2163a49b848cae2dd5746c8
Aaditya <48451686+Aaditya1007@users.noreply.github.com>
2025-03-27 10:44:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import BerserkerGame from './BerserkerGame';
-import './index.css';
 
 function App() {
-  return (
-    <div className="bg-blue-500 p-5">
-  Tailwind color test
-</div>
-  );
+  return <BerserkerGame />;
 }
 
 export default App;
